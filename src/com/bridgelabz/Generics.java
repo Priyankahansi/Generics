c3e46ecc79ea6b2da6b4d9856ef588926e5220e7
Priyanka <priyankahansi95@gmail.com>
2022-05-06 14:55:07 +0530

    added:maximum numbers to find

---
+++ src/com/bridgelabz/Generics.java
@@ -0,0 +1,21 @@
+package com.bridgelabz;
+
+public class Generics {
+
+     public void compareMax(Integer obj1, Integer obj2, Integer obj3)
+     {
+          if(obj1>obj2 && obj1 > obj3)
+               System.out.println(obj1+" "+ "is a maximum number");
+          else if(obj2>obj1 && obj2 > obj3)
+               System.out.println( obj2+" "+" is a maximum number" );
+          else
+               System.out.println(obj3+ " " +"is a maximum number");
+     }
+
+     public static void main(String[] args) {
+          Generics  obj =new Generics();
+          obj.compareMax(8,9,2);
+     }
+}
+
+
