3dd3923d3aadf23a78c5a5bb9d7c45780a4dbff0
Priyanka <priyankahansi95@gmail.com>
2022-05-06 15:06:45 +0530

    added:maximum float number

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -2,20 +2,30 @@ package com.bridgelabz;
 
 public class Generics {
 
-     public void compareMax(Integer obj1, Integer obj2, Integer obj3)
-     {
-          if(obj1>obj2 && obj1 > obj3)
-               System.out.println(obj1+" "+ "is a maximum number");
-          else if(obj2>obj1 && obj2 > obj3)
-               System.out.println( obj2+" "+" is a maximum number" );
-          else
-               System.out.println(obj3+ " " +"is a maximum number");
-     }
+    public void compareMax(Integer obj1, Integer obj2, Integer obj3) {
+        if (obj1 > obj2 && obj1 > obj3)
+            System.out.println(obj1 + " " + "is a maximum number");
+        else if (obj2 > obj1 && obj2 > obj3)
+            System.out.println(obj2 + " " + " is a maximum number");
+        else
+            System.out.println(obj3 + " " + "is a maximum number");
+    }
 
-     public static void main(String[] args) {
-          Generics  obj =new Generics();
-          obj.compareMax(8,9,2);
-     }
+    public void compareFloatMax(Float obj1, Float obj2, Float obj3) {
+        if (obj1 > obj2 && obj1 > obj3)
+            System.out.println(obj1 + " " + "is a maximum number");
+        else if (obj2 > obj1 && obj2 > obj3)
+            System.out.println(obj2 + " " + " is a maximum number");
+        else
+            System.out.println(obj3 + " " + "is a maximum number");
+    }
+
+
+    public static void main(String[] args) {
+        Generics obj = new Generics();
+        obj.compareMax(8, 9, 2);
+        obj.compareFloatMax(1.0f, 2.0f, 3.0f);
+    }
 }
 
 
